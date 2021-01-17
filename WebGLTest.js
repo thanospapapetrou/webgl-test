7079c5a926c017cfa2c054f0c195407e8bfccf37
Athanasios Papapetrou <thanos.papapetrou@gfk.com>
2021-01-17 22:40:11 +0200

    Renaming

--- WebGLTest.js
+++ WebGLTest.js
@@ -1,9 +1,17 @@
 class WebGLTest {
-  static #AMBIENT_LIGHT_COLOR = [0.3, 0.3, 0.3];
-  static #DIRECTIONAL_LIGHT_COLOR = [1, 1, 1];
-  static #DIRECTIONAL_LIGHT_DIRECTION = [0.85, 0.8, 0.75];
+  static AMBIENT_LIGHT_COLOR = [0.3, 0.3, 0.3];
+  static DIRECTIONAL_LIGHT_COLOR = [1, 1, 1];
+  static DIRECTIONAL_LIGHT_DIRECTION = [0.85, 0.8, 0.75];
+  static FAR = 100;
+  static FIELD_OF_VIEW = Math.PI / 4; // rad
+  static NEAR = 0.1;
+  static ROTATION_SPEED = Math.PI; // rad/s
+
+  static #ATTRIBUTE_COLOR = 'color';
+  static #ATTRIBUTE_NORMAL = 'normal';
+  static #ATTRIBUTE_POSITION = 'position';
   static #CANVAS_SELECTOR = '#gl';
-  static #COLOR = 'color';
+
   static #COLORS = [
     // Front face: white
     1.0,  1.0,  1.0,  1.0,
@@ -36,8 +44,6 @@ class WebGLTest {
     1.0,  0.0,  1.0,  1.0,
     1.0,  0.0,  1.0,  1.0
   ];
-  static #FAR = 100;
-  static #FIELD_OF_VIEW = Math.PI / 4; // rad
   static #FRAGMENT_SHADER = `
     varying highp vec3 fragmentLight;
     varying lowp vec4 fragmentColor;
@@ -54,10 +60,6 @@ class WebGLTest {
     16, 17, 18,     16, 18, 19,   // right
     20, 21, 22,     20, 22, 23,   // left
   ];
-  static #MODEL_VIEW = 'modelView';
-  static #NEAR = 0.1;
-  static #NORMAL = 'normal';
-  static #NORMALIZATION = 'normalization';
   static #NORMALS = [
     // Front
      0.0,  0.0,  1.0,
@@ -90,7 +92,6 @@ class WebGLTest {
     -1.0,  0.0,  0.0,
     -1.0,  0.0,  0.0
   ];
-  static #POSITION = 'position';
   static #POSITIONS = [
     // Front face
     -1.0, -1.0,  1.0,
@@ -123,15 +124,16 @@ class WebGLTest {
     -1.0,  1.0,  1.0,
     -1.0,  1.0, -1.0,
   ];
-  static #PROJECTION = 'projection';
-  static #ROTATION_SPEED = Math.PI; // rad/s
   static #UNIFORM_AMBIENT_LIGHT_COLOR = 'ambientLightColor';
   static #UNIFORM_DIRECTIONAL_LIGHT_COLOR = 'directionalLightColor';
   static #UNIFORM_DIRECTIONAL_LIGHT_DIRECTION = 'directionalLightDirection';
+  static #UNIFORM_MODEL_VIEW_MATRIX = 'modelViewMatrix';
+  static #UNIFORM_NORMAL_MATRIX = 'normalMatrix';
+  static #UNIFORM_PROJECTION_MATRIX = 'projectionMatrix';
   static #VERTEX_SHADER = `
-    uniform mat4 modelView;
-    uniform mat4 normalization;
-    uniform mat4 projection;
+    uniform mat4 modelViewMatrix;
+    uniform mat4 normalMatrix;
+    uniform mat4 projectionMatrix;
     uniform vec3 ambientLightColor;
     uniform vec3 directionalLightColor;
     uniform vec3 directionalLightDirection;
@@ -144,16 +146,16 @@ class WebGLTest {
     varying lowp vec4 fragmentColor;
 
     void main(void) {
-      gl_Position = projection * modelView * position;
-      fragmentLight = ambientLightColor + (directionalLightColor * max(dot((normalization * vec4(normal, 1.0)).xyz, normalize(directionalLightColor)), 0.0));
+      gl_Position = projectionMatrix * modelViewMatrix * position;
+      fragmentLight = ambientLightColor + (directionalLightColor * max(dot((normalMatrix * vec4(normal, 1.0)).xyz, normalize(directionalLightColor)), 0.0));
       fragmentColor = color;
     }
   `;
-  static #WEB_GL = 'webgl';
+  static #WEB_GL_CONTEXT = 'webgl';
 
   #gl;
   #program;
-  #projection;
+  #projectionMatrix;
   #positions;
   #normals;
   #colors;
@@ -162,7 +164,7 @@ class WebGLTest {
   #rotation;
 
   static main() {
-    const test = new WebGLTest(document.querySelector(WebGLTest.#CANVAS_SELECTOR).getContext(WebGLTest.#WEB_GL));
+    const test = new WebGLTest(document.querySelector(WebGLTest.#CANVAS_SELECTOR).getContext(WebGLTest.#WEB_GL_CONTEXT));
     requestAnimationFrame(time => test.render(time));
   }
 
@@ -172,19 +174,19 @@ class WebGLTest {
       new VertexShader(gl, WebGLTest.#VERTEX_SHADER),
       new FragmentShader(gl, WebGLTest.#FRAGMENT_SHADER),
       [
-        WebGLTest.#MODEL_VIEW,
-        WebGLTest.#NORMALIZATION,
-        WebGLTest.#PROJECTION,
+        WebGLTest.#UNIFORM_MODEL_VIEW_MATRIX,
+        WebGLTest.#UNIFORM_NORMAL_MATRIX,
+        WebGLTest.#UNIFORM_PROJECTION_MATRIX,
         WebGLTest.#UNIFORM_AMBIENT_LIGHT_COLOR,
         WebGLTest.#UNIFORM_DIRECTIONAL_LIGHT_COLOR,
         WebGLTest.#UNIFORM_DIRECTIONAL_LIGHT_DIRECTION
       ],
       [
-        WebGLTest.#POSITION,
-        WebGLTest.#NORMAL,
-        WebGLTest.#COLOR]
+        WebGLTest.#ATTRIBUTE_POSITION,
+        WebGLTest.#ATTRIBUTE_NORMAL,
+        WebGLTest.#ATTRIBUTE_COLOR]
     );
-    this.#projection = this.#calculateProjection();
+    this.#projectionMatrix = this.#calculateProjectionMatrix();
     this.#positions = new ArrayBuffer(gl, 3, WebGLTest.#POSITIONS);
     this.#normals = new ArrayBuffer(gl, 3, WebGLTest.#NORMALS);
     this.#colors = new ArrayBuffer(gl, 4, WebGLTest.#COLORS);
@@ -202,43 +204,43 @@ class WebGLTest {
     const dt = (time - this.#time);
     this.#time = time;
     this.#gl.clear(this.#gl.COLOR_BUFFER_BIT | this.#gl.DEPTH_BUFFER_BIT);
-    this.#rotation += WebGLTest.#ROTATION_SPEED * dt;
-    const modelView = this.#calculateModelView(this.#rotation);
-    const normalization = this.#calculateNormalization(modelView);
+    this.#rotation += WebGLTest.ROTATION_SPEED * dt;
+    const modelViewMatrix = this.#calculateModelViewMatrix(this.#rotation);
+    const normalMatrix = this.#calculateNormalMatrix(modelViewMatrix);
     this.#program.render(new Map([
-      [WebGLTest.#MODEL_VIEW, modelView],
-      [WebGLTest.#NORMALIZATION, normalization],
-      [WebGLTest.#PROJECTION, this.#projection],
-      [WebGLTest.#UNIFORM_AMBIENT_LIGHT_COLOR, WebGLTest.#AMBIENT_LIGHT_COLOR],
-      [WebGLTest.#UNIFORM_DIRECTIONAL_LIGHT_COLOR, WebGLTest.#DIRECTIONAL_LIGHT_COLOR],
-      [WebGLTest.#UNIFORM_DIRECTIONAL_LIGHT_DIRECTION, WebGLTest.#DIRECTIONAL_LIGHT_DIRECTION],
+      [WebGLTest.#UNIFORM_MODEL_VIEW_MATRIX, modelViewMatrix],
+      [WebGLTest.#UNIFORM_NORMAL_MATRIX, normalMatrix],
+      [WebGLTest.#UNIFORM_PROJECTION_MATRIX, this.#projectionMatrix],
+      [WebGLTest.#UNIFORM_AMBIENT_LIGHT_COLOR, WebGLTest.AMBIENT_LIGHT_COLOR],
+      [WebGLTest.#UNIFORM_DIRECTIONAL_LIGHT_COLOR, WebGLTest.DIRECTIONAL_LIGHT_COLOR],
+      [WebGLTest.#UNIFORM_DIRECTIONAL_LIGHT_DIRECTION, WebGLTest.DIRECTIONAL_LIGHT_DIRECTION],
     ]));
-    this.#positions.render(this.#program.attributes.get(WebGLTest.#POSITION));
-    this.#normals.render(this.#program.attributes.get(WebGLTest.#NORMAL));
-    this.#colors.render(this.#program.attributes.get(WebGLTest.#COLOR));
+    this.#positions.render(this.#program.attributes.get(WebGLTest.#ATTRIBUTE_POSITION));
+    this.#normals.render(this.#program.attributes.get(WebGLTest.#ATTRIBUTE_NORMAL));
+    this.#colors.render(this.#program.attributes.get(WebGLTest.#ATTRIBUTE_COLOR));
     this.#indices.render();
     requestAnimationFrame(time => this.render(time));
   }
 
-  #calculateModelView(rotation) {
-    const modelView = mat4.create();
-    mat4.translate(modelView, modelView, [0.0, 0.0, -10.0]);
-    mat4.rotate(modelView, modelView, rotation, [1, 0, 0]);
-    mat4.rotate(modelView, modelView, rotation, [0, 1, 0]);
-    mat4.rotate(modelView, modelView, rotation, [0, 0, 1]);
-    return modelView;
+  #calculateModelViewMatrix(rotation) {
+    const modelViewMatrix = mat4.create();
+    mat4.translate(modelViewMatrix, modelViewMatrix, [0.0, 0.0, -10.0]);
+    mat4.rotate(modelViewMatrix, modelViewMatrix, rotation, [1, 0, 0]);
+    mat4.rotate(modelViewMatrix, modelViewMatrix, rotation, [0, 1, 0]);
+    mat4.rotate(modelViewMatrix, modelViewMatrix, rotation, [0, 0, 1]);
+    return modelViewMatrix;
   }
 
-  #calculateNormalization(modelView) {
-    const normalization = mat4.create();
-    mat4.invert(normalization, modelView);
-    mat4.transpose(normalization, normalization);
-    return normalization;
+  #calculateNormalMatrix(modelViewMatrix) {
+    const normalMatrix = mat4.create();
+    mat4.invert(normalMatrix, modelViewMatrix);
+    mat4.transpose(normalMatrix, normalMatrix);
+    return normalMatrix;
   }
 
-  #calculateProjection() {
-    const projection = mat4.create();
-    mat4.perspective(projection, WebGLTest.#FIELD_OF_VIEW, this.#gl.canvas.clientWidth / this.#gl.canvas.clientHeight, WebGLTest.#NEAR, WebGLTest.#FAR);
-    return projection;
+  #calculateProjectionMatrix() {
+    const projectionMatrix = mat4.create();
+    mat4.perspective(projectionMatrix, WebGLTest.FIELD_OF_VIEW, this.#gl.canvas.clientWidth / this.#gl.canvas.clientHeight, WebGLTest.NEAR, WebGLTest.FAR);
+    return projectionMatrix;
   }
 }
